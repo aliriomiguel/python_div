509459bb06a42c2e1a5b9da1f37799c4c2b36e66
aliriomiguel <aliriomiguel@aranguren.us>
2023-08-28 18:22:01 +0200

    first commit hola

---
+++ python_div.py
@@ -0,0 +1,11 @@
+def main():
+    try:
+        divisor = float(input("Ingresa un número para dividir 10 entre él: "))
+        result = 10 / divisor
+        print(f"El resultado de la división es: {result}")
+    except ZeroDivisionError:
+        print("Error: No se puede dividir entre cero.")
+    except ValueError:
+        print("Error: Ingresa un valor numérico válido.")
+
+main()
